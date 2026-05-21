16afaba8f9d3a7d5e3bc2726087eefc0bc74076f
maxihan1115-coder <maxihan1115-coder@github.com>
2026-05-21 18:03:38 +0900

    refactor: fr-au-09-login-form-ui-d6 task-7

--- apps/web/src/auth/authStore.ts
+++ apps/web/src/auth/authStore.ts
@@ -32,3 +32,13 @@ export const useAuthStore = create<AuthState & AuthActions>()(
     },
   ),
 )
+
+// 컴포넌트가 store 변화의 정확한 슬라이스만 구독하도록 하는 셀렉터 헬퍼
+export const useAuthUser = () => useAuthStore((s) => s.user)
+export const useIsAuthenticated = () => useAuthStore((s) => s.accessToken !== null)
+export const useAuthActions = () =>
+  useAuthStore((s) => ({
+    setSession: s.setSession,
+    clearSession: s.clearSession,
+    setAccessToken: s.setAccessToken,
+  }))
