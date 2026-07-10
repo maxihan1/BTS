adc9ce3c3a364d92a6cbc6c2eb6738c50fa889d5
maxihan1115-coder <maxihan1115-coder@github.com>
2026-07-10 20:58:30 +0900

    feat: fr-sl-02-d6-slack — Header 설정 메뉴에 Slack 알림 링크 추가

--- apps/web/src/components/Header.tsx
+++ apps/web/src/components/Header.tsx
@@ -182,6 +182,9 @@ export const Header = () => {
             <Link to="/settings/calendar">캘린더 구독</Link>
           </DropdownMenuItem>
           <DropdownMenuItem asChild>
+            <Link to="/settings/slack">Slack 알림</Link>
+          </DropdownMenuItem>
+          <DropdownMenuItem asChild>
             <Link to="/settings/mfa">2단계 인증</Link>
           </DropdownMenuItem>
           <DropdownMenuItem asChild>
